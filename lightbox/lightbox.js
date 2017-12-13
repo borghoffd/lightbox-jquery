88e4f44128127edb2d99a84903261817392de2de
Dennis Borghoff <borghoffd@gmail.com>
2017-12-13 13:16:37 +0100

    changed css classes

--- lightbox/lightbox.js
+++ lightbox/lightbox.js
@@ -4,17 +4,17 @@ jQuery.fn.lightbox = function() {
     $(this).click(function(event) {
         event.preventDefault();
 
-        let overlay = $.parseHTML("<div id='overlay'></div>");
+        let overlay = $.parseHTML("<div id='lightbox__overlay'></div>");
         $("body").append(overlay);
 
-        let image=$.parseHTML("<img id='overlay-img'></img>");
+        let image=$.parseHTML("<img id='lightbox__overlay-img'></img>");
         let src = $(this).attr("href");
         $(image).attr("src",src);
 
-        let container = $.parseHTML("<div id='overlay-container'>");
+        let container = $.parseHTML("<div id='lightbox__overlay-container'>");
         $(container).click(function(event) {
-            $("#overlay-container").remove();
-            $("#overlay").remove();
+            $("#lightbox__overlay-container").remove();
+            $("#lightbox__overlay").remove();
         });
         $(container).append(image);
         $("body").append(container);
@@ -22,8 +22,9 @@ jQuery.fn.lightbox = function() {
 
     $(document).keydown(function(event) {
         if(event.key == "Escape") {
-            $("#overlay-container").remove();
-            $("#overlay").remove();
+            $("#lightbox__overlay-container").remove();
+            $("#lightbox__overlay").remove();
         }
     });
+    return this;
 }
